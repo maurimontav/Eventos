f7ce3bf073575a9e2c668a755777647e39381019
maurimontav <92987612+maurimontav@users.noreply.github.com>
2021-11-26 09:15:32 +0100

    Ejemplo15
    Checkbox.

--- Ejemplo14/src/ejemplo14/Ejemplo14.java
+++ Ejemplo14/src/ejemplo14/Ejemplo14.java
@@ -4,17 +4,95 @@
  */
 package ejemplo14;
 
-/**
- *
- * @author pc
- */
-public class Ejemplo14 {
+/* Importamos las componentes Swing, así como el paquete con los interfaces para los eventos */
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/* La clase JFrame encapsula el concepto de una ventana, para implementar una aplicación que muestre una ventana debemos plantear una clase que herede de la clase JFrame e implemente a a ActionListener para el evento del botón*/
+
+public class Ejemplo14 extends JFrame implements ActionListener{
+
+    /* Definimos variables. */
+
+    private JMenuBar menuBar;
+    private JMenu menu1, menu2, menu3;
+    private JMenuItem menuItem21, menuItem22, menuItem31, menuItem32;
+
+    /* En el constructor de la clase llamamos al método heredado de la clase JFrame llamado setLayout y le pasamos como parámetro un valor null, con esto estamos informándole a la clase JFrame que utilizaremos posicionamiento absoluto para los controles visuales dentro del JFrame*/
+
+    public Ejemplo14() {
+
+        setLayout(null);
+
+        /* Creamos el JMenuBar y lo asociamos con el JFrame */
+
+        menuBar=new JMenuBar();
+        setJMenuBar(menuBar);
+
+        /* Creamos el primer JMenu y lo pasamos como parámetro al JMenuBar mediante el método add */
+
+        menu1=new JMenu("Opciones");
+        menuBar.add(menu1);
+
+        /* Creamos el segundo y tercer objetos de la clase JMenu y los asociamos con el primer JMenu creado */
+
+        menu2=new JMenu("Tamaño de la ventana");
+        menu1.add(menu2);
+        menu3=new JMenu("Color de fondo");
+        menu1.add(menu3);
+
+        /* Creamos los dos primeros objetos de la clase JMenuItem y los asociamos con el segundo JMenu */
+
+        menuItem21=new JMenuItem("640*480");
+        menu2.add(menuItem21);
+        menuItem21.addActionListener(this);
+        menuItem22=new JMenuItem("1024*768");
+        menu2.add(menuItem22);
+        menuItem22.addActionListener(this);
+
+        /* Creamos los otros dos objetos de la clase JMenuItem y los asociamos con el tercer JMenu */
+
+        menuItem31=new JMenuItem("Rojo");
+        menu3.add(menuItem31);
+        menuItem31.addActionListener(this);
+        menuItem32=new JMenuItem("Verde");
+        menu3.add(menuItem32);
+        menuItem32.addActionListener(this);
+
     }
-    
+
+    /* Método que implementa las acciones de cada ítem de menú */
+
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource()==menuItem21) {
+            setSize(640,480);
+        }
+
+        if (e.getSource()==menuItem22) {
+            setSize(1024,768);
+        }
+
+        if (e.getSource()==menuItem31) {
+            getContentPane().setBackground(new Color(255,0,0));
+        }
+
+        if (e.getSource()==menuItem32) {
+            getContentPane().setBackground(new Color(0,255,0));
+        }
+
+    }
+
+    /* En el método main creamos un objeto de la clase Formulario, llamamos al método setBounds para ubicar y dar tamaño al control y mediante el método setVisible hacemos visible el JFrame */
+
+    public static void main(String[] args) {
+
+        Ejemplo14 formulario1=new Ejemplo14();
+        formulario1.setBounds(0,0,300,200);
+        formulario1.setVisible(true);
+
+    }
+
 }
---
+++ Ejemplo15/src/ejemplo15/Ejemplo15.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo15;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Ejemplo15 extends JFrame implements ChangeListener{
+
+    private JCheckBox checkBox1, checkBox2, checkBox3;
+    private JLabel label1;
+
+    public Ejemplo15() {
+
+        setLayout(null);
+
+        //Añado los 3 checkbox con su ChangeListener y la etiqueta
+
+        checkBox1 = new JCheckBox("Español");
+        checkBox1.setBounds(10,10,150,30);
+        checkBox1.addChangeListener(this);
+        add(checkBox1);
+        checkBox2 = new JCheckBox("Inglés");
+        checkBox2.setBounds(10,50,150,30);
+        checkBox2.addChangeListener(this);       
+        add(checkBox2);
+        checkBox3 = new JCheckBox("Alemán");
+        checkBox3.setBounds(10,90,150,30);
+        checkBox3.addChangeListener(this);       
+        add(checkBox3); 
+
+        label1 = new JLabel("Idiomas: ninguno");
+        label1.setBounds(10,130,250,30);
+        add(label1);
+
+    }
+
+    //Implemento el método stateChanged
+
+    public void stateChanged(ChangeEvent e){
+           
+        String texto = "Idiomas: ";
+
+        if (checkBox1.isSelected() == true) {
+            texto += "Español, ";
+        }
+        
+        if (checkBox2.isSelected() == true) {
+            texto += "Ingles, ";
+        }
+        
+        if (checkBox3.isSelected() == true) {
+            texto += "Alemán, ";
+        }
+
+        //Quito la última ',' y escribo el texto
+
+        texto = texto.substring(0, texto.length() - 2);
+   
+        label1.setText(texto);
+
+        }
+
+    public static void main(String[] ar) {
+
+        Ejemplo15 formularioCheckbox = new Ejemplo15();
+        formularioCheckbox.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        formularioCheckbox.setBounds(0,0,300,200);
+        formularioCheckbox.setVisible(true);
+        
+    }   
+
+}
